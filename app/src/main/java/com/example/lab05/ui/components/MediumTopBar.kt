bd27e4adedbe38d951917cb455685389f50d6e22
Lester-Rodrigo <alrodrinew@gmail.com>
2026-08-09 16:04:17 -0600

    Modificación en el padding de TopBar

--- app/src/main/java/com/example/lab05/ui/components/MediumTopBar.kt
+++ app/src/main/java/com/example/lab05/ui/components/MediumTopBar.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
@@ -42,11 +43,12 @@ fun MediumTopBar(
         }
         Text(
             text = topBar.title,
-            fontSize = 30.sp
+            fontSize = 30.sp,
             )
         Text(
             text = topBar.extraText,
-            fontSize = 30.sp
+            fontSize = 20.sp,
+            modifier = Modifier.padding(start = 90.dp)
             )
     }
 }
